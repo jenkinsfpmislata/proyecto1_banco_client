61dd98c1dd5fa02771b0385f4b6a0b80d0305e47
Ivan <Ivan@192.168.0.108>
2014-02-18 19:35:49 +0100

    login cliente

--- web/cliente/login.js
+++ web/cliente/login.js
@@ -4,9 +4,9 @@ app.controller('LoginCtrl', function($scope, $http) {
     $scope.login = null;
     $scope.logeo = function() {
         $http.post("/proyecto1_banco_server/api/Session", $scope.login).success(function(result) {
-            window.location = "index.html";
+            window.location = "../cliente/index.html";
         }, function(error) {
-            alert("Error al introducir el usuario o la contraseña");
+            //alert("Error al introducir el usuario o la contraseña");
         });
     };
     
--- web/cliente/main.js
+++ web/cliente/main.js
@@ -3,10 +3,17 @@ app.controller('MainCtrl', function($scope, $http) {
     $scope.cuentaBancaria = null;
     $scope.movimientosBancarios = null;
 
-    $http.get("/proyecto1_banco_servidor/api/CuentaBancaria/id/1").success(function(r) {
-        $scope.cuentaBancaria = r;
-        $http.get("/proyecto1_banco_servidor/api/CuentaBancaria/" + $scope.cuentaBancaria.idCuentaBancaria + "/MovimientosBancarios").success(function(result) {
-            $scope.movimientosBancarios = result;
-        });
+    /*$http.get("/proyecto1_banco_servidor/api/CuentaBancaria/id/1").success(function(r) {
+     $scope.cuentaBancaria = r;
+     $http.get("/proyecto1_banco_servidor/api/CuentaBancaria/" + $scope.cuentaBancaria.idCuentaBancaria + "/MovimientosBancarios").success(function(result) {
+     $scope.movimientosBancarios = result;
+     });
+     });*/
+
+    $http.get("/CuentaBancaria/1/MovimientosBancarios").success(function(r) {
+        $scope.movimientosBancarios = r;
+        
     });
+    
+    
 });
